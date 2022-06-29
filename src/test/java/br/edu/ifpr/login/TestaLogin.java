abf3715a7c788a3d9c21a0ea961e539df71e03ca
Joile Junior <joilejunior2@gmail.com>
2022-06-29 20:51:30 -0300

    commit inicial

---
+++ src/test/java/br/edu/ifpr/login/TestaLogin.java
@@ -0,0 +1,31 @@
+package br.edu.ifpr.login;
+
+import br.edu.ifpr.paranavai.armarios.controle.LoginControlador;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestaLogin {
+    
+    private String emailInvalido = "blabla@bla.com";
+    private String emailValido = "admin@admin.com";
+    private String senhaValida = "12345";
+    private String senhaInvalida = "";
+    
+    @Test
+    public void autenticacaoOk(){    
+        String resposta = LoginControlador.verifica(emailValido,senhaValida);
+        Assert.assertEquals("Sucesso no login!", resposta);
+    }
+    
+    @Test
+    public void emailInvalido(){
+        String resposta = LoginControlador.verifica(emailInvalido,senhaValida);
+        Assert.assertEquals("Email invalido!", resposta);
+    }
+    
+    @Test
+    public void senhaInvalida(){
+        String resposta = LoginControlador.verifica(emailValido,senhaInvalida);
+        Assert.assertEquals("Senha invalida!", resposta);
+    }
+}
